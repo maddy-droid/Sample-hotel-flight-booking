1cc834c7d02298414ef205567954d11ff690f0fb
Santhosh Ejanthkar <sejanthkar@keypoint-tech.com>
2020-03-02 01:35:41 +0530

    Added type for models

--- app/src/main/java/com/santhu/demo/utils/Constants.java
+++ app/src/main/java/com/santhu/demo/utils/Constants.java
@@ -2,6 +2,9 @@ package com.santhu.demo.utils;
 
 public class Constants {
 
+    public static final int MODEL_TYPE_HOTEL = 1;
+    public static final int MODEL_TYPE_FLIGHT = 2;
+
     public static final String EXTRA_FLIGHT_DETAILS = "extra_flight_details";
 
 }
