3270102b7eb0d19f3e79dcf7218a88793e281e9d
Santhosh Ejanthkar <sejanthkar@keypoint-tech.com>
2020-03-02 16:08:05 +0530

    added show hotels activity and registerted in manifest

---
+++ app/src/main/java/com/santhu/demo/activity/ShowHotelsActivity.java
@@ -0,0 +1,16 @@
+package com.santhu.demo.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowHotelsActivity extends AppCompatActivity {
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
